5695c6b75e1156e39bed7f7be1ab016b400dd29f
JulianDiego <152769609+JulianDiego@users.noreply.github.com>
2023-12-15 18:25:26 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(Options =>
+          Options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+            services.AddSwaggerGen(services =>
+            {
+                services.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "WebApiAutores",
+                    Version = "v1",
+                });
+
+
+            });
+           
+        }
+
+       
+    }
+}
